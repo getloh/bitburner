2c6166641c893c3aba3613e553e6f1c375d80f96
GetLoh <88935273+getloh@users.noreply.github.com>
2022-01-05 10:06:52 +0000

    improvements to stockbot.js

--- stockbot.js
+++ stockbot.js
@@ -1,5 +1,6 @@
 // Stock market bot for bitburner
 // buys and sells stock, hopefully for a profit...
+// version 1.21 - Added check for max stocks, cleaned things up a bit, cycle complete prints less frequently
 
 export async function main(ns) {
     ns.print("Starting script here");
@@ -10,13 +11,12 @@ export async function main(ns) {
     let stockSymbols = ns.stock.getSymbols(); // all symbols
     let portfolio = []; // init portfolio
     const forecastThresh = 0.65; // Buy above this 
-    const minimumCash = 10000000; // Minimum cash in the bank
+    const minimumCash = 50000000; // Minimum cash in the bank
     let cycle = 0;
 
     ns.print("Starting run - Do we own any stocks?"); //Finds and adds any stocks we already own
     for(const stock of stockSymbols){
         let pos = ns.stock.getPosition(stock);
-
         if(pos[0] > 0){
             portfolio.push({sym: stock, value: pos[1], shares: pos[0]})
             ns.print('Detected: '+ stock + ' quant: '+ pos[0] +' @ '+ pos[1]);
@@ -40,13 +40,14 @@ export async function main(ns) {
             }
         } // end of for loop (iterating stockSymbols)
         cycle++;
-        ns.print('Cycle '+ cycle + ' Complete');
+        if (cycle % 5 === 0){   ns.print('Cycle '+ cycle + ' Complete')       };
         await ns.sleep(6000);
     } // end of while true loop
 
     function buyStock(stock) {
         let stockPrice = ns.stock.getAskPrice(stock); // Get the stockprice
-        let shares = stockBuyQuantCalc(stockPrice); // calculate the shares using StockBuyQuantCalc
+        let shares = stockBuyQuantCalc(stockPrice, stock); // calculate the shares to buy using StockBuyQuantCalc
+
         if (ns.stock.getVolatility(stock) <= 0.05){ // if volatility is < 5%, buy the stock
             ns.stock.buy(stock, shares);
             ns.print('Bought: '+ stock + ' quant: '+ Math.round(shares) +' @ '+ Math.round(stockPrice));
@@ -59,17 +60,25 @@ export async function main(ns) {
         let position = ns.stock.getPosition(stock);
         var forecast = ns.stock.getForecast(stock);
         if (forecast < 0.55) {
-            let i = portfolio.findIndex(obj => obj.sym === stock);
+            let i = portfolio.findIndex(obj => obj.sym === stock); //Find the stock info in the portfolio
             ns.print('SOLD: '+ stock + 'quant: '+ portfolio[i].shares +'@ '+ portfolio[i].value);
-            portfolio.splice(i, 1);
+            portfolio.splice(i, 1); // Remove the stock from portfolio
             ns.stock.sell(stock, position[0]);
             
         }
     };
 
-    function stockBuyQuantCalc(stockPrice){ // Calculates how many shares to buy
+    function stockBuyQuantCalc(stockPrice, stock){ // Calculates how many shares to buy
+        
         let playerMoney = ns.getServerMoneyAvailable('home') - minimumCash;
-        let maxSpend = playerMoney * 0.1;
-        return maxSpend/stockPrice
+        let maxSpend = playerMoney * 0.25;
+        let calcShares = maxSpend/stockPrice;
+        let maxShares = ns.stock.getMaxShares(stock);
+        
+        if (calcShares > maxShares){
+            return maxShares
+        }
+        else {return calcShares}
+        
     }
 }
