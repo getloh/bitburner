e7f816f7ef9a3b3c759d55309c1a0ae1625d525c
GetLoh <88935273+getloh@users.noreply.github.com>
2022-01-16 11:35:18 +0000

    stockbot v1.23 - small improvements

--- stockbot.js
+++ stockbot.js
@@ -1,42 +1,48 @@
 // Stock market bot for bitburner - https://danielyxie.github.io/bitburner/
 // Runs infinitely - buys and sells stock, hopefully for a profit...
-// version 1.21 - Added check for max stocks, cleaned things up a bit, cycle complete prints less frequently
+// version 1.23 - Cleaned up prints, changed hardcoded values for editable ones
 
 export async function main(ns) {
-    ns.print("Starting script here");
+    ns.tail();
+    ns.print("Stockbot is booting... WSB here we come");
     ns.disableLog('sleep');
     ns.disableLog('getServerMoneyAvailable');
 
     let stockSymbols = ns.stock.getSymbols(); // all symbols
     let portfolio = []; // init portfolio
     let cycle = 0;
-// ~~~~~~~You can edit these~~~~~~~~
-    const FORECAST_THRESH = 0.65; // Buy above this confidence level (forecast%)
-    const MIN_CASH = 50000000; // Minimum cash to keep
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
+// ~~~~~~~You can edit these~~~~~~~~~~~~~~~~~~
+    const FORECAST_THRESH = 0.65;   // Buy above this confidence level (forecast%)
+    const MIN_CASH = 50000000;      // Minimum cash to keep (50m default)
+    const PROFIT_THRESH = 1.1       // Value should rise above this before being considered for sell (1.1= 10% profit)
+    const FC_SELL_THRESH = 0.55;    // If the above is met, forecast should stay above this or be sold.
+    const STOP_LOSS = 0.4;          // If forecast drops below this, we sell stock (0.4 = 40% chance of increase, 60% chance of decrease)
+    const SPEND_RATIO = 0.25;       // Spends up to this ratio of your total money to buy stocks (minus your min_cash set), (0.25 = 25%)
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    
     ns.print("Starting run - Do we own any stocks?"); //Finds and adds any stocks we already own
     for(const stock of stockSymbols){
         let pos = ns.stock.getPosition(stock);
         if(pos[0] > 0){
-            portfolio.push({sym: stock, value: pos[1], shares: pos[0]})
-            ns.print('Detected: '+ stock + ' quant: '+ pos[0] +' @ '+ pos[1]);
+            portfolio.push({sym: stock, value: pos[1], shares: pos[0]})         
+            ns.print('Detected: '+ stock + ', quant: '+ pos[0] +' @ '+ pos[1]);
         };
     };
 
+    ns.print("all stocks should be added, starting main loop");
     while(true){
-        for(const stock of stockSymbols){ // for each stock symbol
-            if (portfolio.findIndex(obj => obj.sym === stock) !== -1){ //if we already have this stock
-                let i = portfolio.findIndex(obj => obj.sym === stock); // log index of symbol as i
-                if(ns.stock.getAskPrice(stock) >= portfolio[i].value*1.1){ // if the price is higher than what we bought it at +10% then we SELL
+        for(const stock of stockSymbols){                               // for each stock symbol
+            if (portfolio.findIndex(obj => obj.sym === stock) !== -1){  //if we already have this stock
+                let i = portfolio.findIndex(obj => obj.sym === stock);  // log index of symbol as i
+                if(ns.stock.getAskPrice(stock) >= portfolio[i].value * PROFIT_THRESH){ // if the price is higher than what we bought it at +PROFIT_THRESH then we consider SELL
                     sellStock(stock);
                 }
-                else if(ns.stock.getForecast(stock) < 0.4){
+                else if(ns.stock.getForecast(stock) < STOP_LOSS){             // else we check the forecast and dump the stock to stop losses
                     sellStock(stock);
                 }
             }
 
-            else if (ns.stock.getForecast(stock) >= FORECAST_THRESH){ // if the forecast is better than threshold and we don't own then BUY
+            else if (ns.stock.getForecast(stock) >= FORECAST_THRESH){   // if the forecast is better than threshold and we don't own then BUY
                 buyStock(stock);
             }
         } // end of for loop (iterating stockSymbols)
@@ -46,12 +52,12 @@ export async function main(ns) {
     } // end of while true loop
 
     function buyStock(stock) {
-        let stockPrice = ns.stock.getAskPrice(stock); // Get the stockprice
-        let shares = stockBuyQuantCalc(stockPrice, stock); // calculate the shares to buy using StockBuyQuantCalc
+        let stockPrice = ns.stock.getAskPrice(stock);                   // Get the stockprice
+        let shares = stockBuyQuantCalc(stockPrice, stock);              // calculate the shares to buy using StockBuyQuantCalc
 
-        if (ns.stock.getVolatility(stock) <= 0.05){ // if volatility is < 5%, buy the stock
+        if (ns.stock.getVolatility(stock) <= 0.05){                     // if volatility is < 5%, buy the stock
             ns.stock.buy(stock, shares);
-            ns.print('Bought: '+ stock + ' quant: '+ Math.round(shares) +' @ '+ Math.round(stockPrice));
+            ns.print('Bought: '+ stock + ', quant: '+ Math.round(shares) +' @ $'+ Math.round(stockPrice));
 
             portfolio.push({sym: stock, value: stockPrice, shares: shares}); //store the purchase info in portfolio
         }
@@ -59,11 +65,12 @@ export async function main(ns) {
 
     function sellStock(stock) {
         let position = ns.stock.getPosition(stock);
-        var forecast = ns.stock.getForecast(stock);
-        if (forecast < 0.55) {
-            let i = portfolio.findIndex(obj => obj.sym === stock); //Find the stock info in the portfolio
-            ns.print('SOLD: '+ stock + 'quant: '+ portfolio[i].shares +'@ '+ portfolio[i].value);
-            portfolio.splice(i, 1); // Remove the stock from portfolio
+        let forecast = ns.stock.getForecast(stock);
+        let stockPrice = ns.stock.getAskPrice(stock);
+        if (forecast < FC_SELL_THRESH) {
+            let i = portfolio.findIndex(obj => obj.sym === stock);      //Find the stock info in the portfolio
+            ns.print('SOLD: '+ stock + ', quant: '+ Math.round(portfolio[i].shares) +' @ $'+ Math.round(stockPrice) + ' - bought at $' + Math.round(portfolio[i].value));
+            portfolio.splice(i, 1);                                     // Remove the stock from portfolio
             ns.stock.sell(stock, position[0]);
             
         }
@@ -71,13 +78,11 @@ export async function main(ns) {
 
     function stockBuyQuantCalc(stockPrice, stock){ // Calculates how many shares to buy
         let playerMoney = ns.getServerMoneyAvailable('home') - MIN_CASH;
-        let maxSpend = playerMoney * 0.25;
+        let maxSpend = playerMoney * SPEND_RATIO;
         let calcShares = maxSpend/stockPrice;
         let maxShares = ns.stock.getMaxShares(stock);
         
-        if (calcShares > maxShares){
-            return maxShares
-        }
+        if (calcShares > maxShares){return maxShares}
         else {return calcShares}
     }
 }
